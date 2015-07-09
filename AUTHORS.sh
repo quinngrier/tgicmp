1d119d1bcb3060c43db5be3f7e2f8cf254395a48
Quinn Grier <quinn.grier@gmail.com>
2015-07-09 13:08:22 -0500

    Add AUTHORS.sh
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

---
+++ AUTHORS.sh
@@ -0,0 +1,20 @@
+#
+# This script generates an AUTHORS file by using git log. Just add this
+# script to your repository, use it to generate your first AUTHORS file,
+# add that to your repository, and repeat as needed.
+#
+
+set -e
+git log --format='format:%an <%ae>' --all >AUTHORS.tmp
+LC_ALL=C sort -u AUTHORS.tmp >AUTHORS
+exit 0
+
+#
+# The authors of this file have waived all copyright and
+# related or neighboring rights to the extent permitted by
+# law as described by the CC0 1.0 Universal Public Domain
+# Dedication. You should have received a copy of the full
+# dedication along with this file, typically as a file
+# named <CC0-1.0.txt>. If not, it may be available at
+# <https://creativecommons.org/publicdomain/zero/1.0/>.
+#
