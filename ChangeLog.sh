ffcb5c565debc841dac7831ab4f6627b19faafa8
Quinn Grier <quinn.grier@gmail.com>
2015-07-16 22:16:50 -0500

    Use format: instead of tformat: in ChangeLog.sh
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- ChangeLog.sh
+++ ChangeLog.sh
@@ -18,7 +18,7 @@
 set -e
 trap 'rm -f ChangeLog.tmp1 ChangeLog.tmp2 ChangeLog.tmp3' EXIT
 
-x='Commit: %H%nAuthor: %an <%ae>%nDate:   %ad UTC%n%n    %s%n'
+x='format:Commit: %H%nAuthor: %an <%ae>%nDate:   %ad UTC%n%n    %s%n'
 TZ=UTC git log --author-date-order --date=local \
                --pretty="$x" >ChangeLog.tmp1
 sed '/^Author:/s/ <>$//
