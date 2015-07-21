ba77b7e1a19355569425c6e4928c442af0e7c998
Quinn Grier <quinn.grier@gmail.com>
2015-07-21 02:06:20 -0500

    Simplify the sed script in DATE.sh
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- DATE.sh
+++ DATE.sh
@@ -51,7 +51,7 @@ if git ls-files --error-unmatch DATE.sh >/dev/null 2>&1; then
   #
 
   TZ=UTC git log -1 --date=local --pretty=%ad >DATE.tmp
-  x=$(sed 's/... \(...\) \(.\{1,2\}\) ..:..:.. \(....\)/\3-\1-\2/
+  x=$(sed 's/... \(...\) \(.*\) ..:..:.. \(....\)/\3-\1-\2/
            s/Jan/01/; s/Feb/02/; s/Mar/03/; s/Apr/04/
            s/May/05/; s/Jun/06/; s/Jul/07/; s/Aug/08/
            s/Sep/09/; s/Oct/10/; s/Nov/11/; s/Dec/12/
