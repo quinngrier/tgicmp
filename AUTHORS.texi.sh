7b927f002bb7c9585e6bcc46e37b410b03de3c4f
Quinn Grier <quinn.grier@gmail.com>
2015-07-23 00:36:53 -0500

    Add the AUTHORS.texi.fix comments
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- AUTHORS.texi.sh
+++ AUTHORS.texi.sh
@@ -14,6 +14,22 @@
 # be included at the bottom of the AUTHORS.texi file with a blank line
 # preceding it.
 #
+# You can optionally add a file named AUTHORS.texi.fix to sanitize names
+# and email addresses in the AUTHORS.texi file. This is useful to repair
+# strange bytes that would cause the file to not be identified as UTF-8
+# text by file type identification algorithms or to contain ill-formed
+# UTF-8 byte sequences. It can also be used to repair otherwise valid
+# UTF-8 text that produces poor output from Texinfo.
+#
+# The AUTHORS.texi.fix file should contain some awk code that calls the
+# function fix(ere, repl) some number of times. The function behaves
+# like the gsub function and is applied to each name and email address.
+# Here is an example file that will replace each 0x01 or 0x02 byte with
+# the Unicode replacement character (U+FFFD):
+#
+#   fix("\001", "\357\277\275")
+#   fix("\002", "\357\277\275")
+#
 
 set -e
 trap 'for i in 1 2 3; do rm -f AUTHORS.texi.tmp$i; done' EXIT
