b01297f2389aba069c9e290eda96ccf66dd1e20c
Quinn Grier <quinn.grier@gmail.com>
2015-07-24 00:07:42 -0500

    Add the NEWS.sh code
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- NEWS.sh
+++ NEWS.sh
@@ -3,6 +3,15 @@
 # adding housekeeping commands and using makeinfo --plaintext.
 #
 
+set -e
+trap 'rm -f NEWS.tmp1 NEWS.tmp2' EXIT
+echo \\input texinfo >NEWS.tmp1
+echo @setfilename foo >>NEWS.tmp1
+cat NEWS.texi >>NEWS.tmp1
+echo @bye >>NEWS.tmp1
+makeinfo --plaintext NEWS.tmp1 >NEWS.tmp2
+mv NEWS.tmp2 NEWS
+
 #
 # The authors of this file have waived all copyright and
 # related or neighboring rights to the extent permitted by
