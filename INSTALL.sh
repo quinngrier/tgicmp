abebb3530e9ae2bb871ae02475139a886078a843
Quinn Grier <quinn.grier@gmail.com>
2015-07-24 00:09:27 -0500

    Add the INSTALL.sh code
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,6 +3,15 @@
 # file by adding housekeeping commands and using makeinfo --plaintext.
 #
 
+set -e
+trap 'rm -f INSTALL.tmp1 INSTALL.tmp2' EXIT
+echo \\input texinfo >INSTALL.tmp1
+echo @setfilename foo >>INSTALL.tmp1
+cat INSTALL.texi >>INSTALL.tmp1
+echo @bye >>INSTALL.tmp1
+makeinfo --plaintext INSTALL.tmp1 >INSTALL.tmp2
+mv INSTALL.tmp2 INSTALL
+
 #
 # The authors of this file have waived all copyright and
 # related or neighboring rights to the extent permitted by
