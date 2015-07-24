3a064587bff2419ea1b47b5d45156f5a7a962b48
Quinn Grier <quinn.grier@gmail.com>
2015-07-23 20:15:55 -0500

    Use m4_assert in VERSION.sh
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- VERSION.sh
+++ VERSION.sh
@@ -5,7 +5,7 @@
 # your repository and adjust the following code for your configure.ac
 # and Makefile.am:
 #
-#   AC_INIT([foo], m4_esyscmd_s([sh VERSION.sh]))
+#   AC_INIT([foo], m4_esyscmd_s([sh VERSION.sh])m4_assert(!m4_sysval))
 #
 #   EXTRA_DIST += VERSION.sh VERSION
 #
@@ -40,11 +40,7 @@ fi
 
 #
 # If we get here, then we're not running inside the repository and we
-# couldn't read the VERSION file, so we give up. autoconf doesn't care
-# about our exit code, but as long as we don't print anything then it
-# will get mad about being given an empty version string. It would
-# probably not be wise to try to check m4_sysval, as AC_INIT might
-# overwrite it by running other commands.
+# couldn't read the VERSION file, so we give up.
 #
 
 echo 'VERSION.sh: not in repository and VERSION not found' >&2
