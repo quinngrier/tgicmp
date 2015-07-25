0ff616ac25a3a6bf6b5667cb8953c3d6c06e9436
Quinn Grier <quinn.grier@gmail.com>
2015-07-24 20:22:21 -0500

    Edit the first paragraph of AUTHORS.texi.sh
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- AUTHORS.texi.sh
+++ AUTHORS.texi.sh
@@ -1,10 +1,6 @@
 #
-# This script (AUTHORS.texi.sh) creates an AUTHORS.texi file for use in
-# a Texinfo manual by using git log to retrieve the authors induced by
-# HEAD. Just add this script to your repository, use it to create your
-# first AUTHORS.texi file, add that to your repository, and update it
-# when making a commit that is intended to provide a distribution
-# tarball or git archive tarball.
+# This script (AUTHORS.texi.sh) creates an AUTHORS.texi file by using
+# git log to get the chronological list of authors reachable from HEAD.
 #
 # You can optionally add a file named AUTHORS.texi.top, which will be
 # included at the top of the AUTHORS.texi file with a blank line
