4a3de85fa7d7259e7fb5e6c2a2cfb261e2712bfe
Quinn Grier <quinn.grier@gmail.com>
2015-07-26 10:32:57 -0500

    Improve wording
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- AUTHORS.texi.sh
+++ AUTHORS.texi.sh
@@ -65,7 +65,7 @@ cat >AUTHORS.texi.tmp3 <<'EOF'
       print "@item " names[i] " --- @email{" emails[i] "}"
       printf "%d commits", commits[i]
       printf " (%.2f%%)", commits[i] / total * 100
-      printf " over %d", min_years[i]
+      printf " during %d", min_years[i]
       for (y = min_years[i] + 1; y <= max_years[i]; ++y)
         if (!years[i, y]) continue
         else if (!years[i, y - 1]) printf ", %d", y
